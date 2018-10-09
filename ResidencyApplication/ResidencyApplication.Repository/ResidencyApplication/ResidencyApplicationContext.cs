0c85d2bcfedfd81c6d4882eff849f51af97c4874
BookieOandasan <bookieoandasan@gmail.com>
2018-10-09 15:40:01 -0700

    Add files via upload

--- ResidencyApplication/ResidencyApplication.Repository/ResidencyApplication/ResidencyApplicationContext.cs
+++ ResidencyApplication/ResidencyApplication.Repository/ResidencyApplication/ResidencyApplicationContext.cs
@@ -7,12 +7,20 @@ namespace ResidencyApplication.Repository.ResidencyApplication
 {
     public class ResidencyApplicationContext :DbContext
     {
+        public ResidencyApplicationContext(DbContextOptions<ResidencyApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public ResidencyApplicationContext()
+        {
+        }
         public virtual DbSet<ApplicantModel> Applicants { set; get; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ResidencyApplicationDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ResidencyApplication;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
     }
 }
