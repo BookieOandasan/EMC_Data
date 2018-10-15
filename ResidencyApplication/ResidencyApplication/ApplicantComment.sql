70a63d10906d0b2989f781b341ad27e9d131d105
Bookie Oandasan <bookieoandasan@gmail.com>
2018-10-14 19:11:15 -0700

    stuff

---
+++ ResidencyApplication/ResidencyApplication/ApplicantComment.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ApplicantComment]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	applicantId int,
+    [comment] NVARCHAR(MAX) NULL, 
+    [dateCreated] DATETIME NULL, 
+    [dateUpdated] DATETIME NULL, 
+    [createdBy] NVARCHAR(150) NULL,
+	[dateUpdatedBy] NVARCHAR(150) NULL, 
+    CONSTRAINT [FK_ApplicantComment_ToTable] FOREIGN KEY (applicantId) REFERENCES [Applicant](id)
+)
---
+++ ResidencyApplication/ResidencyApplication/ApplicantImage.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[ApplicantImage]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	applicantId int,
+    [image] VARBINARY(50) NULL, 
+    [dateCreated] DATETIME NULL, 
+    [dateUpdated] DATETIME NULL, 
+    [createdBy] NVARCHAR(150) NULL,
+	[dateUpdatedBy] NVARCHAR(150) NULL, 
+    CONSTRAINT [FK_ApplicantImage_ToTable] FOREIGN KEY (applicantId) REFERENCES [Applicant](id)
+)
